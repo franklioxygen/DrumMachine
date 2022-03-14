21d84a30dabda597fcfae11c9708ae2e3d704aa1
Peifan Li <franklioxygen@frankMBA.local>
2022-03-14 16:19:59 -0400

    deploy

---
+++ precache-manifest.17124d789c76c67c8041a3ac23c56645.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89116c9b2e571d05d05af3f098e02202",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4d6b2097ba3c829bfec",
+    "url": "/static/css/main.d507b76f.chunk.css"
+  },
+  {
+    "revision": "b3a1af306e50cb10df16",
+    "url": "/static/js/2.5f5ceea0.chunk.js"
+  },
+  {
+    "revision": "a4d6b2097ba3c829bfec",
+    "url": "/static/js/main.62860795.chunk.js"
+  },
+  {
+    "revision": "547c5b346d26f3275bd6",
+    "url": "/static/js/runtime-main.48b66bbe.js"
+  }
+]);
